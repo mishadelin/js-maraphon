2ad83e503bb00882250850cc22cb28224f69bbdf
it <it@sagarti.com>
2020-09-24 14:06:03 +0300

    с утро не внимательно прочитал задание. Сделал, что бы просто считал колличество нужных букв в введенной строке. Дописал что бы выводил строку где больше нужных букв

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-function words() {
+    const firstRow = 'мама мыла раму';
+    const secondRow = 'собака друг человека';
+
+function getRow(firstRow, secondRow) {
+    
     var k = 0;
+    var s = 0;
     var stringsearch = ["а", "a"];
-    var Row = prompt("Введите текст:");
-    for (var i = 0; i < Row.length; i++){
+    // var Row = prompt("Введите текст:");
+    for (var i = 0; i < firstRow.length; i++){
         for (var j = 0; j < stringsearch.length; j++){
-            if (Row[i] === stringsearch[j]) {
+            if (firstRow[i] === stringsearch[j]) {
                 ++k;
             }
         }
     }
+    for (var i = 0; i < secondRow.length; i++){
+        for (var j = 0; j < stringsearch.length; j++){
+            if (secondRow[i] === stringsearch[j]) {
+                ++s;
+            }
+        }
+    }
+    if (k > s){
+        alert (firstRow);
+    } else {
+        alert (secondRow);
+    }
     return k ? k : "нет совпадений";
 }
 
-alert(words());
+console.log(getRow(firstRow, secondRow)); 
